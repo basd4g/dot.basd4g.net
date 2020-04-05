d21784d78c05493e9a23ec0c3570908b7f5163bd
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-04-05 19:17:48 +0900

    Rename redirectURL file name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const http = require('http');
 
 const port = process.env.PORT || 8080;
-const redirectURL = "https://raw.githubusercontent.com/basd4g/dotfiles/master/bin/install.sh"
+const redirectURL = "https://raw.githubusercontent.com/basd4g/dotfiles/master/bin/download.sh"
 
 const server = http.createServer((req, res) => {
   res.writeHead(302, {
